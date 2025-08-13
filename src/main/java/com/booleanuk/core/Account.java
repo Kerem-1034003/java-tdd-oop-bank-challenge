b3687b0a865b3eabde9ba5360fae912cfe1eb9c8
Your Name <kerem-1034003>
2025-08-13 13:34:15 +0200

    user story 3 Green: test passed

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -2,15 +2,52 @@ package com.booleanuk.core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public abstract class Account {
     protected List<Transaction> transactions;
+    protected double balance;
 
     public Account(){
         this.transactions = new ArrayList<>();
+        this.balance = 0;
     }
 
     public List<Transaction> getTransactions(){
         return transactions;
     }
+
+    public void deposit(double amount, String date){
+        balance+= amount;
+        transactions.add(new Transaction(date, amount, "deposit"));
+    }
+
+    public void withdraw(double amount, String date){
+        balance -= amount;
+        transactions.add(new Transaction(date, amount, "withdraw"));
+    }
+
+    public List<String> getStatement() {
+        List<String> statement = new ArrayList<>();
+        double runningBalance = balance;
+
+        for (int i = transactions.size() - 1; i >= 0; i--) {
+            Transaction t = transactions.get(i);
+            String line;
+
+            if (t.getType().equals("deposit")) {
+                line = String.format(Locale.US, "%s || %.2f || %.2f", t.getDate(), t.getAmount(), runningBalance);
+                runningBalance -= t.getAmount();
+            } else if (t.getType().equals("withdraw")) {
+                line = String.format(Locale.US, "%s || %.2f || %.2f", t.getDate(), t.getAmount(), runningBalance);
+                runningBalance += t.getAmount();
+            } else {
+                continue;
+            }
+            statement.add(line);
+        }
+
+        return statement;
+        }
+
 }
--- src/main/java/com/booleanuk/core/Transaction.java
+++ src/main/java/com/booleanuk/core/Transaction.java
@@ -1,4 +1,25 @@
 package com.booleanuk.core;
 
 public class Transaction {
+    private final String date;
+    private final double amount;
+    private final String type;
+
+    public Transaction(String date, double amount, String type){
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
