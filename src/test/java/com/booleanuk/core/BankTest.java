c2c4ff60427509f4f77e1890a7467ad1253eb536
Your Name <kerem-1034003>
2025-08-13 10:34:24 +0200

    user story 1 Red: Test

---
+++ src/test/java/com/booleanuk/core/BankTest.java
@@ -0,0 +1,22 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTest {
+
+    @Test
+    public void createCurrentAccount(){
+        CurrentAccount account = new CurrentAccount();
+        assertNotNull(account, "Account created");
+    }
+
+    @Test
+    public void newAccount_ShouldHaveNoTransactions(){
+        CurrentAccount account = new CurrentAccount();
+        List<Transaction> transactions = account.getTransactions();
+        assertTrue(transactions.isEmpty(), "new account with no Transactions");
+
+    }
+}
